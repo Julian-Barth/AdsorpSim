85917754410b016e7c41d166dc3403b270f5c3bd
Julian-Barth <julian.barth@epfl.ch>
2025-04-25 17:46:28 +0200

    reverted

--- tests/test_example_module.py
+++ tests/test_example_module.py
@@ -1,6 +1,6 @@
-#from adsorpsim.example_module import hello_smiles
+from adsorpsim.example_module import hello_smiles
 
 
-# Test the function
-#def test_hello_smiles():
-#    assert hello_smiles("C(=O)O") == "Hello, C(=O)O!", "Test failed: SMILES input"
+#Test the function
+def test_hello_smiles():
+    assert hello_smiles("C(=O)O") == "Hello, C(=O)O!", "Test failed: SMILES input"
