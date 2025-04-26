2e0ed0759040c62b5624a42d7055c3919ddb4835
Julian-Barth <julian.barth@epfl.ch>
2025-04-26 15:43:02 +0200

    preparing UI

--- src/adsorpsim/__init__.py
+++
@@ -1,9 +0,0 @@
-"""A package to model the adsorption of atmospheric CO2."""
-
-from __future__ import annotations
-
-from .core import Bed, Adsorbent_Langmuir
-
-__all__ = ["Bed", "Adsorbent_Langmuir"]
-
-__version__ = "0.0.1"
