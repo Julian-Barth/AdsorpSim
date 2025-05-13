4e6ccb7fb058bfc9ac7ff759a320c0463d56a074
Julian-Barth <julian.barth@epfl.ch>
2025-05-13 15:46:08 +0200

    changed the init call functions

--- src/adsorpsim/__init__.py
+++ src/adsorpsim/__init__.py
@@ -4,4 +4,14 @@ from __future__ import annotations
 
 from .functions import langmuir_adsorption_carbon
 
+from .core import (
+    Adsorbent_Langmuir,
+    Bed,
+    download_data,
+    add_adsorbent_to_list,
+    get_percentage_point,
+    get_adsorbed_quantity,
+    plot_the_graph
+)
+
 __version__ = "0.0.1"
