a3b9da1e917ded59a11140804cfa88ba644aa83d
Julian-Barth <julian.barth@epfl.ch>
2025-05-13 16:28:25 +0200

    passing tox

--- src/adsorpsim/__init__.py
+++ src/adsorpsim/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import annotations
 
-from .core import Adsorbent_Langmuir, Bed, simulate, download_data, get_percentage_point, plot_the_graph, add_adsorbent_to_list, get_adsorbed_quantity
+from .core import Adsorbent_Langmuir, Bed, download_data, get_percentage_point, plot_the_graph, add_adsorbent_to_list, get_adsorbed_quantity
 
 __version__ = "0.0.1"
