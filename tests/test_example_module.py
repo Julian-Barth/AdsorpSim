d838485d0ee4fe3934f47d4a11284cbf3a7e0ae8
Julian-Barth <julian.barth@epfl.ch>
2025-05-14 11:39:18 +0200

    adding tests

--- tests/test_example_module.py
+++ tests/test_example_module.py
@@ -2,8 +2,14 @@ import pytest
 import numpy as np
 
 from adsorpsim import (
-    Adsorbent_Langmuir, Bed, get_percentage_point,
-    get_adsorbed_quantity
+    Adsorbent_Langmuir,
+    Bed,
+    download_data,
+    get_percentage_point,
+    plot_the_graph,
+    add_adsorbent_to_list,
+    get_adsorbed_quantity,
+    langmuir_adsorption_carbon
 )
 
 @pytest.fixture
